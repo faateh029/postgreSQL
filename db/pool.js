9e5098831a7a43a0d50deac1e2aa0c1face3197f
faateh029 <buttfatih691@gmail.com>
2025-07-24 11:50:11 +0500

    Added dotenv package and seperated sensitive info to .env file

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,11 @@
 import {Pool} from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pool = new Pool({
-    host: "localhost" , 
-    user: "postgres",
-    password:"123",
-    database:"top_users",
-    port:5432
+    host:process.env.DB_HOST,  
+    user: process.env.DB_USER ,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DATABASE,
+    port:process.env.PORT
 });
